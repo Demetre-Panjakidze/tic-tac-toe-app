875d89f3e7f612b6126d656ea0c2a67ebc002991
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-04-05 01:30:56 +0400

    homepage almost done

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent {
-
+  oClicked: boolean = true;
+  xClick() {
+    this.oClicked = false;
+  }
+  oClick() {
+    this.oClicked = true;
+  }
+  vsCpu() {
+    console.log(this.oClicked);
+  }
+  vsPlayer() {
+    console.log(this.oClicked);
+  }
 }
